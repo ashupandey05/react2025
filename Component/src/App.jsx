9a81e46c1e7fe79f01144c0f38f53e400688687b
Ashutosh <ashutosh.ashu.0520@gmail.com>
2024-12-26 21:04:38 +0530

    dynamic value insertion in components

--- Component/src/App.jsx
+++ Component/src/App.jsx
@@ -15,18 +15,23 @@ function App() {
   );
 }
 const Netflixseries = () => {
+  const name = "Money Heist";
+  const summary =
+    "When the national mint and a touring school group are held hostage by robbers, police believe that the thieves have no way out. Little do theyknow that the thieves have a bigger plan in store.";
+  
+  const getGenre = ()=>{
+    const genre = "Thriller";
+    return genre;
+  }
   return (
     <>
       <div>
         <img src="img12.jpeg" alt="money heist" height="30%" width="40%" />
       </div>
-      <h2>Name: Money Heist</h2>
-      <h3>Rating: 8.2</h3>
-      <p>
-        When the national mint and a touring school group are held hostage by
-        robbers, police believe that the thieves have no way out. Little do they
-        know that the thieves have a bigger plan in store.
-      </p>
+      <h2>Name: {name}</h2>
+      <h3>Rating:{5 + 3.2}</h3>
+      <p>{summary}</p>
+      <p>Genre: {getGenre()}</p>
     </>
   );
 };
